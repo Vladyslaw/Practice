304a08e11a7f3e66b219253167e873de09deffa8
Vladislav <test>
2017-09-25 09:07:37 +0300

    Monday's commit

--- MyPractice/src/Practice.java
+++ MyPractice/src/Practice.java
@@ -3,5 +3,7 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello on Wednesday 2017-09-20!");
+
+        System.out.println("Hello on Monday 2017-09-25!");
     }
 }
